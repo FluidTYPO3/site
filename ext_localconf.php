8053872088ca2ee649cc1cc9ea5ec4ab3109ee25
Göran Bodenschatz <coding@46halbe.de>
2014-11-28 22:23:14 +0100

    [TASK] Adapt slot signature for CMS 7
    There is a change in the signature of the dispatched Signal 'afterExtensionConfigurationWrite' in CMS 7. An additional parameter $extensionKey comes in as first one.

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,9 +5,18 @@ if (!defined('TYPO3_MODE')) {
 
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('site', 'volatileinstaller.php');
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
-	'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
-	'afterExtensionConfigurationWrite',
-	'VolatileInstaller',
-	'process'
-);
+if ('6.2' === TYPO3_branch) {
+	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
+		'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
+		'afterExtensionConfigurationWrite',
+		'VolatileInstaller',
+		'processForSixTwo'
+	);
+} else {
+	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
+		'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
+		'afterExtensionConfigurationWrite',
+		'VolatileInstaller',
+		'process'
+	);
+}
