1d070965ca1e82e304555f8ef88024a34670dc48
Claus Due <claus@namelesscoder.net>
2015-11-07 17:21:06 +0100

    [DOC] Fix notation about package relationship

--- Classes/Service/EnterpriseLevelEnumeration.php
+++ Classes/Service/EnterpriseLevelEnumeration.php
@@ -2,7 +2,7 @@
 namespace FluidTYPO3\Site\Service;
 
 /*
- * This file is part of the FluidTYPO3/Vhs project under GPLv2 or later.
+ * This file is part of the FluidTYPO3/Site project under GPLv2 or later.
  *
  * For the full copyright and license information, please read the
  * LICENSE.md file that was distributed with this source code.
