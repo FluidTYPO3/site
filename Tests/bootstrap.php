fb950bfaa2e37ce5fb10c98ea855fc66a8bde5ca
Claus Due <claus@namelesscoder.net>
2015-11-07 18:52:30 +0100

    [TASK] Adapt to new testing strategy

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,8 +10,12 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('FluidTYPO3\\Site\\Tests\\Unit\\', __DIR__ . '/Unit/');
 $autoloader->addPsr4('TYPO3\\CMS\\Core\\', __DIR__ . '/../vendor/typo3/cms/typo3/sysext/core/Classes/');
+$autoloader->addPsr4('TYPO3\\CMS\\Core\\Tests\\', __DIR__ . '/../vendor/typo3/cms/typo3/sysext/core/Tests/');
 $autoloader->addPsr4('TYPO3\\CMS\\Extbase\\', __DIR__ . '/../vendor/typo3/cms/typo3/sysext/extbase/Classes/');
 
-\FluidTYPO3\Development\Bootstrap::initialize(array(
-	'cache_core' => \FluidTYPO3\Development\Bootstrap::CACHE_PHP_NULL,
-));
+\FluidTYPO3\Development\Bootstrap::initialize(
+	$autoloader,
+	array(
+		'cache_core' => \FluidTYPO3\Development\Bootstrap::CACHE_PHP_NULL,
+	)
+);
